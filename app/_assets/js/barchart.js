f3729a69e53933b18ce3c9d9dab808f129a76486
Martin Chorley <me@martinjc.com>
2016-12-22 10:53:18 +0000

    barchart working

--- app/_assets/js/barchart.js
+++ app/_assets/js/barchart.js
@@ -3,7 +3,7 @@ function barChart() {
     var width = 600;
     var height = 400;
 
-    var padding = {top: 10, bottom: 200, left: 100, right: 10};
+    var padding = {top: 10, bottom: 200, left: 80, right: 10};
 
     var data = [];
 
@@ -12,7 +12,7 @@ function barChart() {
     var x_property = 'x';
     var y_property = 'y';
 
-    var colour_scale = d3.scale.quantile()
+    var colour_scale = d3.scaleQuantile()
             .range(colorbrewer.Reds[7])
             .domain(d3.extent(data, function(d){ return d[y_property]; }));
 
@@ -27,10 +27,11 @@ function barChart() {
     function chart(selection) {
         selection.each(function(){
 
-            var x_scale = d3.scale.ordinal()
-                .rangeRoundBands([padding.left, width-padding.right], 0.1)
-                .domain(data.map(function(d){ return d[x_property]; }));
-            var y_scale = d3.scale.linear()
+            var x_scale = d3.scaleBand()
+                .rangeRound([padding.left, width-padding.right], 0.1)
+                .domain(data.map(function(d){ return d[x_property]; }))
+                .paddingInner(0.1);
+            var y_scale = d3.scaleLinear()
                 .range([height - padding.bottom, padding.top])
                 .domain([0, d3.max(data, function(d){ return d[y_property]; })]);
 
@@ -66,81 +67,19 @@ function barChart() {
                 .attr("transform", "translate(" + padding.left + ",0)");
 
             if(y_label) {
-                svg.select('.y.axis')
-                    .append('text')
+                svg.append('text')
                     .attr('class', 'y label')
-                    .attr("text-anchor", "middle")
-                    .attr("dx", -(height-padding.bottom)/2)
-                    .attr("dy", "-3em")
-                    .attr("transform", "rotate(-90)");
             }
 
-            var x_axis = d3.svg.axis()
-                  .scale(x_scale)
-                  .orient("bottom");
+            var x_axis = d3.axisBottom(x_scale)
 
-            var y_axis = d3.svg.axis()
-              .scale(y_scale)
-              .orient("left")
-              .ticks(8);
+            var y_axis = d3.axisLeft(y_scale)
+              .tickArguments([8]);
 
             draw = function() {
 
                 var update = svg.selectAll('rect.bar')
                     .data(data);
-
-                update
-                    .enter()
-                    .append('rect')
-                    .attr('class', 'bar')
-                    .attr('y', function(d){ return height-padding.bottom; })
-                    .attr('x', function(d){ return x_scale(d[x_property]); })
-                    .attr('height', function(d){ return 0; })
-                    .attr('width', function(d){ return x_scale.rangeBand(); })
-                    .on("mouseover", function(d){
-                        if(tooltip_function) {
-                            tooltip.html(tooltip_function(d));
-                            tooltip_active = true;
-                            tooltip.style("visibility", "visible");
-                        }
-                    })
-                    .on("mousemove", function(){
-                        if(tooltip_function) {
-                            tooltip
-                                .style("top", (d3.event.pageY-10)+"px")
-                                .style("left",(d3.event.pageX+10)+"px");
-                        }
-
-                    })
-                    .on("mouseout", function(){
-                        if(tooltip_function) {
-                            tooltip_active = false;
-                            tooltip.style("visibility", "hidden");
-                        }
-                    })
-                    .on("click", function(d){
-                        if(tooltip_function) {
-                            if(tooltip_active) {
-                                tooltip.style("visibility", "hidden");
-                                tooltip_active = false;
-                            } else {
-                                tooltip.html(tooltip_function(d));
-                                tooltip.style("visibility", "visible");
-                                tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");
-                                tooltip_active = true;
-                            }
-                        }
-                    });
-
-                update
-                    .transition()
-                    .duration(transition_duration)
-                    .attr('x', function(d){ return x_scale(d[x_property]); })
-                    .attr('y', function(d){ return y_scale(d[y_property]); })
-                    .attr('height', function(d){ return height - padding.bottom - y_scale(d[y_property]); })
-                    .attr('width', function(d){ return x_scale.rangeBand(); })
-                    .attr('fill', function(d){ return colour_scale(d[y_property]); });
-
 
                 update
                     .exit()
@@ -151,6 +90,56 @@ function barChart() {
                     .attr('height', function(d){ return 0; })
                     .attr('width', function(d){ return x_scale.rangeBand(); })
                     .remove();
+
+                update
+                    .enter()
+                        .append('rect')
+                        .attr('class', 'bar')
+                    .merge(update)
+                        .attr('y', function(d){ return height-padding.bottom; })
+                        .attr('x', function(d){ return x_scale(d[x_property]); })
+                        .attr('width', function(d){ return x_scale.bandwidth(); })
+                        .on("mouseover", function(d){
+                            if(tooltip_function) {
+                                tooltip.html(tooltip_function(d));
+                                tooltip_active = true;
+                                tooltip.style("visibility", "visible");
+                            }
+                        })
+                        .on("mousemove", function(){
+                            if(tooltip_function) {
+                                tooltip
+                                    .style("top", (d3.event.pageY-10)+"px")
+                                    .style("left",(d3.event.pageX+10)+"px");
+                            }
+
+                        })
+                        .on("mouseout", function(){
+                            if(tooltip_function) {
+                                tooltip_active = false;
+                                tooltip.style("visibility", "hidden");
+                            }
+                        })
+                        .on("click", function(d){
+                            if(tooltip_function) {
+                                if(tooltip_active) {
+                                    tooltip.style("visibility", "hidden");
+                                    tooltip_active = false;
+                                } else {
+                                    tooltip.html(tooltip_function(d));
+                                    tooltip.style("visibility", "visible");
+                                    tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");
+                                    tooltip_active = true;
+                                }
+                            }
+                        })
+                    .transition()
+                    .duration(transition_duration)
+                    .attr('x', function(d){ return x_scale(d[x_property]); })
+                    .attr('y', function(d){ return y_scale(d[y_property]); })
+                    .attr('height', function(d){ return height - padding.bottom - y_scale(d[y_property]); })
+                    .attr('width', function(d){ return x_scale.bandwidth(); })
+                    .attr('fill', function(d){ return colour_scale(d[y_property]); });
 
                 svg.select(".x.axis")
                     .transition()
@@ -171,12 +160,11 @@ function barChart() {
 
                 if(y_label) {
                     svg.select(".y.label")
-                        .transition()
-                        .duration(transition_duration)
-                        .attr("text-anchor", "middle")
-                        .attr("dx", -(height-padding.bottom)/2)
-                        .attr("dy", "-3em")
                         .attr("transform", "rotate(-90)")
+                        .attr("text-anchor", "middle")
+                        .attr("font-size", "12px")
+                        .attr("dx", -(height-padding.bottom)/2)
+                        .attr("dy", "2em")
                         .text(y_label);
                 }
 
